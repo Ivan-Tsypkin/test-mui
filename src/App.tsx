ddefdc1f054a75d64cc74f2d927a361f133b1b0e
Ivan <bfg546@yandex.ru>
2023-10-18 14:52:24 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+// import { useState } from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const App = () => {
+  // const [count, setCount] = useState(0);
+
+  console.log('test');
+
+  return <Container>hi</Container>;
+};
+
+export default App;
